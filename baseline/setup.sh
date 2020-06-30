fe4d830f8ad8495d7900be8eb289f2f3ddb57d94
tangcheng1 <tangcheng1@sina.weibo.com>
2020-06-30 19:34:53 +0800

    re-baking this repo, updated opensuse from 42.3 to opensuse leap 15.1, and php5 => php7

--- baseline/setup.sh
+++ baseline/setup.sh
@@ -3,10 +3,22 @@
 set -e
 set -x
 
+#zypper ls
+#exit
+
+zypper ar -f http://mirrors.163.com/openSUSE/update/openSUSE-current/ 163-update
+zypper ar -f http://mirrors.163.com/openSUSE/update/openSUSE-non-oss-current/ 163-update-non-oss
+zypper ar -f http://mirrors.163.com/openSUSE/distribution/leap/15.1/repo/oss/ 163-oss
+zypper ar -f http://mirrors.163.com/openSUSE/distribution/leap/15.1/repo/non-oss/ 163-non-oss
+
+zypper modifyrepo -d repo-oss repo-update
+
+zypper ref
+
 # Add repositories
-zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/languages:/php/openSUSE_Leap_42.3/ php
-zypper --non-interactive ar http://download.opensuse.org/repositories/home:/marec2000:/nodejs/openSUSE_Leap_42.3/ nodejs
-zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_Leap_42.3/ python
+zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/languages:/php/openSUSE_Leap_15.1/ php
+#zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/languages:/nodejs/openSUSE_Leap_15.1/ nodejs
+zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_Leap_15.1/ python
 
 # Install Git before we add the SCM repository (the SCM repository contains Git 2.11, which is broken).
 zypper --gpg-auto-import-keys --non-interactive in --force-resolution git
@@ -22,15 +34,18 @@ if [ "$(git --version)" == *"2.11"* ]; then
 fi
 
 # Add SCM package for other tools (Subversion, Mercurial)...
-zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/tools:/scm/openSUSE_Leap_42.3/ scm
+zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/tools:/scm/openSUSE_Leap_15.1/ scm
 
 # Install requirements
-zypper --gpg-auto-import-keys --non-interactive in --force-resolution nginx php-fpm php5-mbstring php5-mysql php5-curl php5-pcntl php5-gd php5-openssl php5-ldap php5-fileinfo php5-posix php5-json php5-iconv php5-ctype php5-zip php5-sockets which python3-Pygments nodejs ca-certificates ca-certificates-mozilla ca-certificates-cacert sudo subversion mercurial php5-xmlwriter nodejs-ws php5-opcache ImageMagick postfix glibc-locale supervisor
+zypper --gpg-auto-import-keys --non-interactive in --force-resolution nginx php7 php7-fpm php php7-pecl php7-mbstring php7-mysql php7-curl php7-pcntl php7-gd php7-openssl php7-ldap php7-fileinfo php7-posix php7-json php7-iconv php7-ctype php7-zip php7-sockets which python3-Pygments ca-certificates ca-certificates-mozilla ca-certificates-cacert sudo subversion mercurial php7-xmlwriter php7-opcache ImageMagick postfix glibc-locale supervisor
 
 # Build and install APCu
-zypper --non-interactive install --force-resolution autoconf automake binutils cpp cpp48 gcc gcc48 glibc-devel libasan0 libatomic1 libcloog-isl4 libgomp1 libisl10 libitm1 libltdl7 libmpc3 libmpfr4 libpcre16-0 libpcrecpp0 libpcreposix0 libstdc++-devel libstdc++48-devel libtool libtsan0 libxml2-devel libxml2-tools linux-glibc-devel m4 make ncurses-devel pcre-devel php5-devel php5-pear php5-zlib pkg-config readline-devel tack xz-devel zlib-devel
-printf "\n" | pecl install apcu-4.0.10
-#zypper --non-interactive remove --force-resolution autoconf automake binutils cpp cpp48 gcc gcc48 glibc-devel libasan0 libatomic1 libcloog-isl4 libgomp1 libisl10 libitm1 libltdl7 libmpc3 libmpfr4 libpcre16-0 libpcrecpp0 libpcreposix0 libstdc++-devel libstdc++48-devel libtool libtsan0 libxml2-devel libxml2-tools linux-glibc-devel m4 ncurses-devel pcre-devel php5-devel php5-pear pkg-config readline-devel tack xz-devel zlib-devel
+zypper --non-interactive install --force-resolution autoconf automake binutils cpp gcc glibc-devel libatomic1 libgomp1 libitm1 libltdl7 libmpc3 libpcre16-0 libpcrecpp0 libpcreposix0 libstdc++-devel libtool libtsan0 libxml2-devel libxml2-tools linux-glibc-devel m4 make ncurses-devel pcre-devel php7-devel php7-pear php7-zlib pkg-config readline-devel tack xz-devel zlib-devel
+
+pecl channel-update pecl.php.net
+
+printf "\n" | pecl install apcu-5.1.18
+#zypper --non-interactive remove --force-resolution autoconf automake binutils cpp cpp48 gcc gcc48 glibc-devel libasan0 libatomic1 libcloog-isl4 libgomp1 libisl10 libitm1 libltdl7 libmpc3 libmpfr4 libpcre16-0 libpcrecpp0 libpcreposix0 libstdc++-devel libstdc++48-devel libtool libtsan0 libxml2-devel libxml2-tools linux-glibc-devel m4 ncurses-devel pcre-devel php7-devel php7-pear pkg-config readline-devel tack xz-devel zlib-devel
 
 # Remove cached things that pecl left in /tmp/
 rm -rf /tmp/*
